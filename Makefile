cf46806342ca3e6315059563e080a7aefde903db
Arnd Bergmann <arnd@arndb.de>
2011-02-16 18:17:37 +0100

    flashbench: add an erase tool
    This trivial tool can be used to erase block devices
    using the BLKDISCARD ioctl. The tool does nothing
    but calling that ioctl with the command line arguments.
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- Makefile
+++ Makefile
@@ -2,13 +2,15 @@ CC	:= gcc
 CFLAGS	:= -O2 -Wall -Wextra -Wno-missing-field-initializers -Wno-unused-parameter -g2
 LDFLAGS := -lrt
 
-all: flashbench
+all: flashbench erase
 
 dev.o: dev.c dev.h
 vm.o: vm.c vm.h dev.h
 flashbench.o: flashbench.c vm.h dev.h
 
 flashbench: flashbench.o dev.o vm.o
+
+erase: erase.o
 
 clean:
 	rm flashbench flashbench.o dev.o vm.o
---
+++ erase.c
@@ -0,0 +1,38 @@
+#define _GNU_SOURCE
+
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <linux/fs.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int fd = open(argv[1], O_RDWR | O_DIRECT);
+	int ret;
+	unsigned long long range[2];
+
+	if (argc != 4) {
+		fprintf(stderr, "usage: %d <device> <start> <length>\n",
+			argv[0]);
+	}
+
+	if (fd < 0) {
+		perror("open");
+		return errno;
+	}
+
+	range[0] = atoll(argv[2]);
+	range[1] = atoll(argv[3]);
+
+	printf("erasing %lld to %lld on %s\n", range[0], range[0] + range[1], argv[1]);
+
+	ret = ioctl(fd, BLKDISCARD, range);
+	if (ret)
+		perror("ioctl");
+
+	return errno;
+}
